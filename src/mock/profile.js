0601defe5227c82d4c97f9a74dbaea99f7163aa7
Kedar Labde <labde.kedar@gmail.com>
2024-08-27 00:12:59 +0530

    init

---
+++ src/mock/profile.js
@@ -0,0 +1,23 @@
+export const SOCIAL_LINKS = [
+  {
+    link: 'https://twitter.com/dhavalCode',       
+    icon: 'akar-icons:twitter-fill',        
+  },
+  {
+    link: 'https://github.com/dhavalcode',
+    icon: 'akar-icons:github-fill',
+  },
+  {
+    link: 'https://linkedin.com/in/dhavalcode',
+    icon: 'akar-icons:linkedin-fill',
+  },
+];
+
+export const HERO_TITLES = ['Dhaval Patel', 'Full-stack Engineer', 'AI Enthusiast'];
+
+export const aboutParagraph =
+  'Highly motivated and enthusiastic Full Stack Engineer with experience in designing, developing and maintaining web applications using technologies such as Generative AI, Typescript, React, Next.js, Node.js, Python.';
+
+export const contactEmail = 'works.dhaval@gmail.com';
+
+export const documentTitle = 'Hello I am Dhaval Patel | Full Stack Engineer | AI Enthusiast'
