b001c7a5fd710117fda843522a37a511ee8b72f6
Kedar Labde <labde.kedar@gmail.com>
2024-08-27 00:56:28 +0530

    changes started

--- src/mock/profile.js
+++ src/mock/profile.js
@@ -1,23 +1,23 @@
 export const SOCIAL_LINKS = [
+  // {
+  //   link: 'https://www.instagram.com/me.kedar/',       
+  //   icon: 'akar-icons:github-fill',        
+  // },
   {
-    link: 'https://twitter.com/dhavalCode',       
-    icon: 'akar-icons:twitter-fill',        
-  },
-  {
-    link: 'https://github.com/dhavalcode',
+    link: 'https://github.com/kedar-labde',
     icon: 'akar-icons:github-fill',
   },
   {
-    link: 'https://linkedin.com/in/dhavalcode',
+    link: 'https://www.linkedin.com/in/kedar-labde-64931ab4/',
     icon: 'akar-icons:linkedin-fill',
   },
 ];
 
-export const HERO_TITLES = ['Dhaval Patel', 'Full-stack Engineer', 'AI Enthusiast'];
+export const HERO_TITLES = ['Kedar Labde', 'Lead Android Engineer', 'Kotlin Enthusiast'];
 
 export const aboutParagraph =
-  'Highly motivated and enthusiastic Full Stack Engineer with experience in designing, developing and maintaining web applications using technologies such as Generative AI, Typescript, React, Next.js, Node.js, Python.';
+  'Experienced Android Developer with over 9.5 years of expertise in mobile application development. Proficient in Kotlin and Jetpack Compose, with a strong focus on crafting high-quality, scalable applications. Skilled in implementing modern UI/UX designs, optimizing performance, and adhering to SOLID principles. Adept at utilizing Dagger for dependency injection, Coroutines for asynchronous programming, and integrating with various APIs and services. Proven track record in leading projects, collaborating with cross-functional teams, and delivering robust solutions that enhance user experiences.';
 
-export const contactEmail = 'works.dhaval@gmail.com';
+export const contactEmail = 'me.kedarlabde@gmail.com';
 
-export const documentTitle = 'Hello I am Dhaval Patel | Full Stack Engineer | AI Enthusiast'
+export const documentTitle = 'Hello I am Kedar Labde | Lead Android Engineer | Kotlin Enthusiast'
